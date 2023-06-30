11a8abebbf89bb62d01dfc03bbc2c4dcd2927df4
John4850 <johnnelson4850@gmail.com>
2023-06-29 20:58:52 -0700

    lesson 1-3

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { PageWrapper } from "./home.styles"
 import { Header, GifViewer, Button } from "../../components"
 import { useGif } from "../../data/hooks/useGif"
@@ -14,10 +14,21 @@ export const HomePage = () => {
 		isError,
 	} = useGif(DEFAULT_KEYWORD)
 
+	const handleClick = ()=> {
+		fetchNewGif()
+	};
+
+	useEffect(()=>{
+
+	},[gif, fetchNewGif])
+
+	let buttonText = "Start"
+
 	return (
 		<PageWrapper>
 			<Header>React Telly</Header>
-			<GifViewer />
+			<GifViewer gifUrl={gif?.data.images.downsized_medium.url} isError={isError} isLoading={isLoading} />
+			<Button onClick={handleClick} >{buttonText}</Button>
 		</PageWrapper>
 	)
 }
